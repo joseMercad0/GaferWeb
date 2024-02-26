cedb44d6021d065393e678fd5f002c02843e12b5
joseMercad0 <jose.mercado.c@tecsup.edu.pe>
2024-02-26 11:37:53 -0500

    header added

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,3 +10,27 @@ const loadElement = document.querySelector("[data-preloader]");
 window.addEventListener("load", function () {
   loadElement.classList.add("loaded");
 });
+
+const navbar = document.querySelector("[data-navbar]");
+const navToggler = document.querySelector("[data-nav-toggler]");
+
+const toggleNavbar = function () {
+    navbar.classList.toggle("active");
+    navToggler.classList.toggle("active");
+}
+
+navToggler.addEventListener("click", toggleNavbar);
+
+/**
+ *  #HEADER
+ */
+
+const header = document.querySelector("[data-header]");
+
+window.addEventListener("scroll", function() {
+    if (window.scrollY >=  100) {
+        header.classList.add("active");
+    } else {
+        header.classList.remove("active");
+    }
+});
