0fa16e9ddb69ffe3bf70bb42db083518fb052c83
joseMercad0 <jose.mercado.c@tecsup.edu.pe>
2024-03-05 12:57:49 -0500

    Actualización propuesta servicio

--- assets/js/script.js
+++ assets/js/script.js
@@ -57,3 +57,15 @@ const scrollReveal = function () {
 
 window.addEventListener("scroll", scrollReveal);
 window.addEventListener("load", scrollReveal);
+
+
+/**
+ * expand-circle
+ */
+
+const img = document.querySelector("img");
+    const icons = document.querySelector(".icons");
+    img.onclick = function(){
+      this.classList.toggle("active");
+      icons.classList.toggle("active");
+    }
